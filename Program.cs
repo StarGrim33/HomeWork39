eafa4c389d366f3b88121ea518281b167ba7847b
StarGrim33 <prosky33@gmail.com>
2022-07-10 11:39:24 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace JuniorHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string importantInfo = "qwerty123456";
+            string password = "babyonmoretime";
+            int tryCount = 3;
+            string userInput;
+
+            for (int i = 0; i < tryCount; i ++)
+            {
+                Console.WriteLine("Введите пароль для доступа к секретной информации: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Пароль для доступа к секретной инфрмации: " + importantInfo);
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("У вас осталось " + (tryCount - i - 1) + " попыток");
+                }
+            }
+        }
+    }
+}
